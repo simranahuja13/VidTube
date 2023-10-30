654c8921c5359ad699f0bf071797b237e259c1e5
Simran <officialsimranahuja@gmail.com>
2023-10-30 14:11:11 +0530

    added files

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const menuIcon = document.querySelector('.menu-icon');
 const sidebar = document.querySelector('.sidebar');
+const container = document.querySelector('.container');
  menuIcon.addEventListener('click' , function(){
     sidebar.classList.toggle("small-sidebar");
+    container.classList.toggle("large-container");
  })
+
 
